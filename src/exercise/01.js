b79d62ed671d52012a3ce82bdfd237f366fbe80c
Stephanie Chan <103970034+eina-chan@users.noreply.github.com>
2022-07-28 15:47:53 +1000

    corrections

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -74,20 +74,20 @@ function useUser() {
 }
 
 // 🐨 add a function here called `updateUser`
-function updateUser({userDispatch, user, updates}) {
-  userDispatch({type: 'start update', updates: formState})
-  userClient.updateUser(user, formState).then(
-    updatedUser => userDispatch({type: 'finish update', updatedUser}),
-    error => userDispatch({type: 'fail update', error}),
+function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  return userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
   )
 }
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
 
-// export {UserProvider, useUser}
+// export {UserProvider, useUser, updateUser}
 
 // src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
+// import {UserProvider, useUser, updateUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -104,7 +104,7 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    updateUser({userDispatch, user, updates})
+    updateUser(userDispatch, user, formState)
   }
 
   return (
