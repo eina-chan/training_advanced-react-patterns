138d1cce0d784cdd37e13dbc5a120d94fb9d19db
Stephanie Chan <103970034+eina-chan@users.noreply.github.com>
2022-07-29 11:39:23 +1000

    add callAll fn

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,6 +4,15 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      // Will only call function if it exists
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
@@ -12,10 +21,7 @@ function useToggle() {
   function getTogglerProps({onClick, ...props} = {}) {
     return {
       'aria-pressed': on,
-      onClick: () => {
-        onClick && onClick()
-        toggle()
-      },
+      onClick: callAll(onClick, toggle),
       ...props,
     }
   }
